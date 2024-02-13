e8e4dfca52dc7ee5c43eb096744320db7e9d144f
saraiesco <saraiescobar@google.com>
2024-02-13 14:19:47 -0600

    useStates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.scss';
 import { useState } from 'react';
+import bear from './assets/bear.jpg';
 
 function App() {
+  const[noCount,setCount]=useState(0);
+  const[yesPressed, setYesPressed]=useState(false);
+  const yesButtonSize = noCount*20 +16;
+
+  const phrases=[
+    "No",
+    "Are you sure?",
+    "Really sure?",
+    "Boogs please",
+    "Pretty please",
+    "nooooooooo ;("
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <b><p>
           Valentines
         </p></b>
+      <img className='bear' src={bear} alt='bear'/>
         <p>Will you be my Valentine?</p>
       </header>
     </div>
